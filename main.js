5af248cd51f6dc4c050b075253114a8ee2f8bbaf
MaKafui0 <139414083+MaKafui0@users.noreply.github.com>
2024-01-12 20:29:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser() {
+    player1_name = documment.getElementById(player1_login_input)
+    player2_name = documment.getElementById(player2_login_input)
+    localStorage.setItem("player1_name".player1_name);
+    localStorage.setItem("player1_name".player1_name);
+    window.location="index.html"
+};
